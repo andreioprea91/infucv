0dc757d27847bdad03ae58a84f8a9e35e5dc990b
andreioprea91 <andrei.oprea91@yahoo.com>
2019-12-16 16:55:35 +0200

    quizz logic almost dose

--- public/quizzLogic.js
+++ public/quizzLogic.js
@@ -1,98 +1,78 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
+const startButton = document.getElementById("start-btn");
+const nextButton = document.getElementById("next-btn");
 
-const buttOne=document.getElementById('b1');
-const buttwo=document.getElementById('b2');
-const butthree=document.getElementById('b3');
-const buttfour=document.getElementById('b4');
-const questionElement = document.getElementById('question')
+const buttOne = document.getElementById("b1");
+const buttwo = document.getElementById("b2");
+const butthree = document.getElementById("b3");
+const buttfour = document.getElementById("b4");
+const questionElement = document.getElementById("question");
 
-const result=document.getElementById('correct')
-let questions, currentQuestionIndex
+const result = document.getElementById("correct");
+let questions, currentQuestionIndex;
 
-
-
-
-
-
-fetch('/quizz/data')
+fetch("/quizz/data")
   .then(res => {
     return res.json();
   })
   .then(loadedQuestions => {
-    
     questions = loadedQuestions;
-    
   })
   .catch(err => {
     console.error(err);
   });
 
-  startGame = () => {
-    currentQuestionIndex = 0
-    startButton.innerText="Restart"
-    
-    setNextQuestion();
-  };
+startGame = () => {
+  currentQuestionIndex = 0;
+  startButton.innerText = "Restart";
 
+  setNextQuestion();
+};
 
-  function setNextQuestion() {
-    //resetState()
-    showQuestion(questions[currentQuestionIndex])
-  }
+function setNextQuestion() {
+  showQuestion(questions[currentQuestionIndex]);
+}
 
+function showQuestion(question) {
+  questionElement.innerText = question.question;
+  const shuffledAnswers = question.answers.sort(() => Math.random() - 0.5);
+  buttOne.innerText = shuffledAnswers[0];
+  buttwo.innerText = shuffledAnswers[1];
+  butthree.innerText = shuffledAnswers[2];
+  buttfour.innerText = shuffledAnswers[3];
 
-  function showQuestion(question) {
-  questionElement.innerText = question.question
-  const shuffledAnswers = question.answers.sort(() => Math.random() - .5)
-  buttOne.innerText = shuffledAnswers[0]
-  buttwo.innerText = shuffledAnswers[1]
-  butthree.innerText = shuffledAnswers[2]
-  buttfour.innerText = shuffledAnswers[3]
-  
-  buttOne.addEventListener('click', () => {
-    if (buttOne.innerText===question.correct_answer){
-      result.innerText="correct"
-    }else{
-      result.innerText="False"
+  buttOne.addEventListener("click", () => {
+    if (buttOne.innerText === question.correct_answer) {
+      result.innerText = "correct";
+    } else {
+      result.innerText = "False";
     }
-  })
-  buttwo.addEventListener('click', () => {
-    if (buttwo.innerText===question.correct_answer){
-      result.innerText="correct"
-    }else{
-      result.innerText="False"
+  });
+  buttwo.addEventListener("click", () => {
+    if (buttwo.innerText === question.correct_answer) {
+      result.innerText = "correct";
+    } else {
+      result.innerText = "False";
     }
-  })
-  butthree.addEventListener('click', () => {
-    if (butthree.innerText===question.correct_answer){
-      result.innerText="correct"
-    }else{
-      result.innerText="False"
+  });
+  butthree.addEventListener("click", () => {
+    if (butthree.innerText === question.correct_answer) {
+      result.innerText = "correct";
+    } else {
+      result.innerText = "False";
     }
-  })
-  buttfour.addEventListener('click', () => {
-    if (buttfour.innerText===question.correct_answer){
-      result.innerText="correct"
-    }else{
-      result.innerText="False"
+  });
+  buttfour.addEventListener("click", () => {
+    if (buttfour.innerText === question.correct_answer) {
+      result.innerText = "correct";
+    } else {
+      result.innerText = "False";
     }
-  })
+  });
+}
 
-
-  
-
-  }
-
-  
-
-
-
-startButton.addEventListener('click', startGame)
-nextButton.addEventListener('click', () => {
-  result.innerText=''
-  currentQuestionIndex++
-  setNextQuestion()
-})
-
- 
+startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+  result.innerText = "";
+  currentQuestionIndex++;
+  setNextQuestion();
+});
