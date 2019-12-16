5dfa370322e84790caadb478fc590d30f0b04f63
andreioprea91 <andrei.oprea91@yahoo.com>
2019-12-16 16:49:05 +0200

    quizz logic almost done

--- public/quizzLogic.js
+++ public/quizzLogic.js
@@ -1,12 +1,12 @@
 const startButton = document.getElementById('start-btn')
 const nextButton = document.getElementById('next-btn')
-const questionContainerElement = document.getElementById('question-container')
+
 const buttOne=document.getElementById('b1');
 const buttwo=document.getElementById('b2');
 const butthree=document.getElementById('b3');
 const buttfour=document.getElementById('b4');
 const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+
 const result=document.getElementById('correct')
 let questions, currentQuestionIndex
 
@@ -50,13 +50,47 @@ fetch('/quizz/data')
   butthree.innerText = shuffledAnswers[2]
   buttfour.innerText = shuffledAnswers[3]
   
-       
+  buttOne.addEventListener('click', () => {
+    if (buttOne.innerText===question.correct_answer){
+      result.innerText="correct"
+    }else{
+      result.innerText="False"
+    }
+  })
+  buttwo.addEventListener('click', () => {
+    if (buttwo.innerText===question.correct_answer){
+      result.innerText="correct"
+    }else{
+      result.innerText="False"
+    }
+  })
+  butthree.addEventListener('click', () => {
+    if (butthree.innerText===question.correct_answer){
+      result.innerText="correct"
+    }else{
+      result.innerText="False"
+    }
+  })
+  buttfour.addEventListener('click', () => {
+    if (buttfour.innerText===question.correct_answer){
+      result.innerText="correct"
+    }else{
+      result.innerText="False"
+    }
+  })
+
+
+  
+
   }
+
+  
 
 
 
 startButton.addEventListener('click', startGame)
 nextButton.addEventListener('click', () => {
+  result.innerText=''
   currentQuestionIndex++
   setNextQuestion()
 })
